99c3c3757efa3b37ce288bcb1cd0ac8e8cc5b921
Paul Imoke <pauleke65@gmail.com>
2020-12-28 13:05:41 +0000

    Added Services

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -1,7 +1,52 @@
 <template>
 
 <section class="bd-grid items-center" id="services">
+
+  <div class="flex pt-8 flex-col items-center justify-center">
+
+      <div class="rounded-full py-3 px-12 mx-auto w-64 h-auto font-bold mt-8 text-2xl bg-white text-black text-center">Services</div>
    
+   <div class=" flex pt-4 flex-col items-center justify-center w-full">
+
+     <div class="mt-12">
+       <div>
+         <img class="mx-auto h-96 " src="../assets/images/iPhone 12 Pro Max.svg" alt="">
+       </div>
+       <div class="text-white pt-4 text-center text-2xl">
+         <p>Mobile App</p>
+         <p>Development</p>
+       </div>
+     </div>
+
+      <div class="mt-12">
+       <div>
+         <img class="mx-auto " src="../assets/images/MacBook Pro.svg" alt="">
+       </div>
+       <div class="text-white text-center text-2xl">
+         <p>Website Development</p>
+       </div>
+     </div>
+
+     <div class="mt-12">
+       <div>
+         <img class="mx-auto h-52" src="../assets/images/branding.svg" alt="">
+       </div>
+       <div class="text-white pt-4 text-center text-2xl">
+         <p>Branding</p>
+       </div>
+     </div>
+
+     <div class="mt-12">
+       <div>
+         <img class="mx-auto" src="../assets/images/design.svg" alt="">
+       </div>
+       <div class="text-white text-center text-2xl">
+         <p>Design</p>
+       </div>
+     </div>
+
+   </div>
+  </div>
 </section>
 
 
