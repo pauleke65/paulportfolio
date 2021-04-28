b4a0c850d00291393c93408ab16d35bc7a8587ae
Paul Imoke <pauleke65@gmail.com>
2021-04-28 08:20:11 +0000

    re-added project pages

---
+++ src/views/Project.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="pt-8 px-48 flex flex-col h-screen">
+
+        <div class="flex mb-8">
+            <img class="h-96"
+            src="https://is5-ssl.mzstatic.com/image/thumb/Purple113/v4/5b/9a/a8/5b9aa818-b65d-3e00-ede6-8e57b54708b5/pr_source.png/230x0w.webp" 
+            alt=""
+            >
+
+            <div class="flex flex-col ml-8">
+            <h2 class="font-bold text-2xl">Trello: Organize Anything</h2>
+
+             <div class="flex">
+                <p class="my-1 mr-2">Built with ❤ using:</p>
+                <img
+                class="my-1 mr-2 w-6"
+                src="../assets/images/tailwindcss.svg"
+                alt=""
+              />
+              <img
+                class="my-1 mr-2 w-6"
+                src="../assets/images/graphql.svg"
+                alt=""
+              />
+              <img
+                class="my-1 mr-2 w-6 h-6"
+                src="../assets/logo.png"
+                alt=""
+              />
+
+            </div>
+
+        <p class="mt-2 text-justify">Organize anything with anyone—for free!
+
+Trello is a visual tool for organizing your work and life. Join more than 35 million registered users to organize all your projects at work, at home, or anywhere in between.
+
+Whether you’re planning a website design project, vacation, or company off-site, Trello is infinitely customizable and flexible for your every need.</p>
+
+  <a href="https://www.google.com" class="">Test It Out</a>
+  
+            </div>
+        </div>
+
+        <hr>
+        <h3 class="my-4 font-semibold text-xl">Screenshots</h3>
+        <div class="flex">
+            
+        </div>
+
+    </div>
+</template>
---
+++ src/views/Projects.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex flex-col ">
+
+      <h1 class="text-6xl font-extrabold ml-6 mt-6">All Projects</h1>
+
+<div class="h-screen pt-6 px-6 md:px-64">
+    
+    <div class="flex my-4">
+        <div class="flex-initial">
+            <img class="w-36 h-36 mr-6" src="https://is5-ssl.mzstatic.com/image/thumb/Purple124/v4/a8/fd/89/a8fd898e-5b3a-9deb-f2c4-60b9027a5860/AppIcon-0-1x_U007emarketing-0-7-0-0-85-220.png/350x350.png" alt="">
+        </div>
+        <div class="flex-1">
+            <h2 class="font-semibold text-lg md:text-3xl">Trello: Organize Anything!</h2>
+            <p class="mt-2 ">Organize anything with anyone—for free! Trello is a visual tool for organizing your work and life. Join more than 35 million...</p>
+            <div class="flex">
+                <p class="my-1 mr-2">Built with ❤ using:</p>
+                <img
+                class="my-1 mr-2 w-6"
+                src="../assets/images/tailwindcss.svg"
+                alt=""
+              />
+              <img
+                class="my-1 mr-2 w-6"
+                src="../assets/images/graphql.svg"
+                alt=""
+              />
+              <img
+                class="my-1 mr-2 w-6"
+                src="../assets/logo.png"
+                alt=""
+              />
+
+            </div>
+            <a class=" text-blue-500" href="/project">Find Out More Here ></a>
+        </div>
+    </div>
+
+     <div class="flex my-4">
+        <div class="flex-initial">
+            <img class="w-36 h-36 mr-6" src="https://is4-ssl.mzstatic.com/image/thumb/Purple124/v4/a7/07/d4/a707d463-bdd2-9b96-84fb-899dc851af36/AppIcon-1x_U007emarketing-0-7-0-85-220.png/350x350.png" alt="">
+            </div>
+            <div class="flex-1">
+            <h2 class="font-semibold text-3xl">Smartsheet: Teams and Projects</h2>
+            <p class="mt-2 mb-4">Featured “App of the Day” on the App Store. “This project management is powerful but not overwhelming—and never lets the process...</p>
+            <a class=" text-blue-500" href="/project">Find Out More Here ></a>
+        </div>
+    </div>
+</div>
+   
+  </div>
+</template>
